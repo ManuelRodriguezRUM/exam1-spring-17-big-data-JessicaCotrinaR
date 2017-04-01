873ca8d78eefe98436c3321c0bc58502d31fb7bf
Jessica Cotrina <jessica.cotrina@upr.edu>
2017-04-01 16:31:52 -0400

    Agregando proyecto

--- hive/hive.sql
+++ hive/hive.sql
@@ -1,1 +1,23 @@
 # You sql follows
+
+Select region, ciudad, count(*)
+from escuelaspr
+group by region, ciudad;
+
+
+select sc.identificacion, count(*)
+from studentspr e, escuelaspr sc
+where e.identificacion_escuela = sc.identificacion
+group by sc.identificacion;
+
+
+select identificacion_student, escuelaspr.nombre
+from escuelaspr, studentspr
+where escuelaspr.identificacion = studentspr.identificacion_escuela 
+and (escuelaspr.ciudad = “Ponce” or escuelaspr.ciudad = “Cabo Rojo”);
+
+
+select es.region, es.ciudad, count(*)
+from studentspr st, escuelaspr es
+where st.identificacion_escuela = es.identificacion
+group by es.region, es.ciudad; 
--- spark/p3b.py
+++ spark/p3b.py
@@ -1,1 +1,28 @@
 #Code here
+# Para correr el ejercicio
+#pyspark --master yarn --deploy-mode client --conf="sparkexecutorEnv.PYTHONHASHSEED=223"
+
+import csv
+
+escuela_file = sc.textFile("/home/jessica/escuelasPR.csv")
+escuela_datos=escuela_file.map(lambda x: [x]).map(lambda x : list(csv.reader(x))[0])
+ponce = escuela_datos.filter(lambda x: x[2] == 'Ponce')
+cabo_rojo = escuela_datos.filter(lambda x: x[2] == 'Cabo Rojo')
+dorado = escuela_datos.filter(lambda x: x[2] == 'Dorado')
+resultado = ponce.union(Cabo_Rojo).union(Dorado)
+resultado.foreach(print)
+
+# Resultados
+"""
+['Ponce', 'Ponce', 'Ponce', '52274', 'SUPERIOR JUAN SERRALL�S', 'Superior', '1587']
+['Ponce', 'Ponce', 'Ponce', '52514', 'PONCE HIGH SCHOOL', 'Superior', '1570']
+['Ponce', 'Ponce', 'Ponce', '52688', 'DR. PILA', 'Superior', '1575']
+['Ponce', 'Ponce', 'Ponce', '52696', 'BERNARDINO CORDERO BERNARD', 'Superior', '1576']
+['Ponce', 'Ponce', 'Ponce', '52712', 'THOMAS ARMSTRONG TORO', 'Superior', '1585']
+['Ponce', 'Ponce', 'Ponce', '56069', 'BETHZAIDA VELAZQUEZ ANDUJAR', 'Superior', '1583']
+['Ponce', 'Ponce', 'Ponce', '56432', 'SUP. JARDINES DE PONCE', 'Superior', '1586']
+['Ponce', 'Ponce', 'Ponce', '58511', 'LILA MAR�A MERCEDES MAYORAL', 'Superior', '1580']
+['Mayag�ez', 'Cabo Rojo', 'Cabo Rojo', '46821', 'IN�S MAR�A MENDOZA DE MU�OZ MARIN', 'Superior', '1120']
+['Mayag�ez', 'Cabo Rojo', 'Cabo Rojo', '46987', 'MONSERRATE LE�N DE IRIZARRY', 'Superior', '1125']
+['Arecibo', 'Vega Alta', 'Dorado', '71092', 'JOS� SANTOS ALEGR�A', 'Superior', '1240']
+"""
